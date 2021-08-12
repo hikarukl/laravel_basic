8a2ca5aac99b0bac4e18addde1a4ae5368490b16
DuongVu <duong.vu@dayone.vn>
2021-08-12 20:45:34 +0700

    Add number views

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\PostShowRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Services\Post as PostService;
+use App\Services\Category as CategoryService;
 
 class PostController extends Controller
 {
@@ -13,22 +14,29 @@ class PostController extends Controller
      * @var PostService
      */
     protected $postService;
+    /**
+     * @var CategoryService
+     */
+    protected $categoryService;
 
-    public function __construct(PostService $postService)
+    public function __construct(PostService $postService, CategoryService $categoryService)
     {
         $this->postService = $postService;
+        $this->categoryService = $categoryService;
     }
 
     public function show(PostShowRequest $request, $slug)
     {
         try {
             $data = [
-                'post'         => $this->postService->detailBySlug($slug),
-                'recent_posts' => $this->postService->getRecentPosts()
+                'post'          => $this->postService->detailBySlug($slug),
+                'recent_posts'  => $this->postService->getRecentPosts(),
+                'category_list' => $this->categoryService->getAllCategory()
             ];
 
             // Increase view number
-
+            $data['post']->number_views += 1;
+            $data['post']->save();
 
             return view('pages.post.show', $data);
         } catch (\Exception $e) {
